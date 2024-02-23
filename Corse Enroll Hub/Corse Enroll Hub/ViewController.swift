85c8dae8f4b76d5ae8d98490fa58d73844038a79
Teja Kumar Muppala <tejakumarmuppala02@gmail.com>
2024-02-23 11:24:38 -0600

    Created Project app in xcode

---
+++ Corse Enroll Hub/Corse Enroll Hub/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Corse Enroll Hub
+//
+//  Created by Teja Kumar Muppala on 2/23/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Corse Enroll Hub/Corse Enroll HubUITests/Corse_Enroll_HubUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Corse_Enroll_HubUITestsLaunchTests.swift
+//  Corse Enroll HubUITests
+//
+//  Created by Teja Kumar Muppala on 2/23/24.
+//
+
+import XCTest
+
+final class Corse_Enroll_HubUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
