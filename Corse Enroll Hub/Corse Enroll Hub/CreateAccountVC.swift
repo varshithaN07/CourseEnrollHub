ba017aee8336534632e7e02b3b683823fec03f65
varshithanalluri <varshithanalluri7@gmail.com>
2024-02-23 12:42:16 -0600

    Created loginVC and  Added connections to login page

---
+++ Corse Enroll Hub/Corse Enroll Hub/CreateAccountVC.swift
@@ -0,0 +1,29 @@
+//
+//  CreateAccountVC.swift
+//  Corse Enroll Hub
+//
+//  Created by Varshitha Nalluri on 2/23/24.
+//
+
+import UIKit
+
+class CreateAccountVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Corse Enroll Hub/Corse Enroll Hub/LoginVC.swift
@@ -0,0 +1,34 @@
+//
+//  LoginVC.swift
+//  Corse Enroll Hub
+//
+//  Created by Varshitha Nalluri on 2/23/24.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    
+    @IBOutlet weak var loginEmailTF: UITextField!
+    @IBOutlet weak var loginpasswordTF: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func loginletsgoBTN(_ sender: UIButton) {
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
