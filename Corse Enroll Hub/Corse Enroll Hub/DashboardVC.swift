b78d3abe12ab40de03d1c34907e09e0548cafb6e
AGRAJA <agraja.fall2023@gmail.com>
2024-03-07 23:14:42 -0600

    Agraja : Created DashboardVC and added custom Table view as per requirement.

---
+++ Corse Enroll Hub/Corse Enroll Hub/DashboardVC.swift
@@ -0,0 +1,30 @@
+//
+//  DashboardVC.swift
+//  Corse Enroll Hub
+//
+//  Created by AGRAJA GOTTIPATI on 3/7/24.
+//
+
+import UIKit
+
+class DashboardVC: UIViewController {
+
+    @IBOutlet weak var table: UITableView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
