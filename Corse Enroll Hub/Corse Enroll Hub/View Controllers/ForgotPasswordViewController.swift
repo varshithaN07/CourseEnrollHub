c8976fee9f2ef1d387dc9c9f43c1f65c7489bc12
varshithanalluri <varshithanalluri7@gmail.com>
2024-03-29 18:51:23 -0500

    Varshitha Nalluri : added a ForgotPasswordVC and created a view with required UI Elements

---
+++ Corse Enroll Hub/Corse Enroll Hub/View Controllers/ForgotPasswordViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ForgotPasswordViewController.swift
+//  Corse Enroll Hub
+//
+//  Created by Varshitha Nalluri on 3/29/24.
+//
+
+import UIKit
+
+class ForgotPasswordViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
