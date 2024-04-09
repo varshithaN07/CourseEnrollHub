6afcb772dce96d787efe4b6aa55c310b9c0c9cf7
varshithanalluri <varshithanalluri7@gmail.com>
2024-04-08 23:58:17 -0500

    Varshitha: C reated CourseRegisterVC scene and Outlets for that scene

---
+++ Corse Enroll Hub/Corse Enroll Hub/CourseRegister'ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  CourseRegister'ViewController.swift
+//  Corse Enroll Hub
+//
+//  Created by Varshitha Nalluri on 4/8/24.
+//
+
+import UIKit
+
+class CourseRegister_ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
