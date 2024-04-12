f3347d8bbbf880da119c8d1d87df0626385b9877
Khalida Parveen Mohammad <mohammadkhalidaparveen23@gmail.com>
2024-04-12 17:05:42 -0500

    Made changes in ForgotPasswordViewController

--- Corse Enroll Hub/Corse Enroll Hub/View Controllers/ForgotPasswordViewController.swift
+++ Corse Enroll Hub/Corse Enroll Hub/View Controllers/ForgotPasswordViewController.swift
@@ -9,15 +9,54 @@ import UIKit
 
 class ForgotPasswordViewController: UIViewController {
 
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var feedbackLabel: UILabel!
+    var passwordResetService: PasswordResetService?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        feedbackLabel.text = ""
     }
     
     @IBAction func cancel(_ sender: Any) {
         self.dismiss(animated: true)
     }
+    
+    @IBAction func resetPassword(_ sender: Any) {
+        guard let email = emailTextField.text, !email.isEmpty else {
+            feedbackLabel.text = "Please enter your email address."
+            return
+        }
+        
+        guard isValidEmail(email) else {
+            feedbackLabel.text = "Please enter a valid email address."
+            return
+        }
+        
+        performPasswordReset(for: email)
+    }
+    
+    func isValidEmail(_ email: String) -> Bool {
+        let emailRegex = "[A-Z0-9a-z._%+-]+@[A-Z0-9a-z.-]+\\.[A-Za-z]{2,}"
+        let predicate = NSPredicate(format: "SELF MATCHES %@", emailRegex)
+        return predicate.evaluate(with: email)
+    }
+    
+    func performPasswordReset(for email: String) {
+        passwordResetService?.resetPassword(for: email) { [weak self] success, error in
+            DispatchQueue.main.async {
+                if success {
+                    self?.feedbackLabel.text = "Password reset link has been sent to \(email)."
+                } else if let error = error {
+                    self?.feedbackLabel.text = "Failed to send password reset link: \(error.localizedDescription)."
+                } else {
+                    self?.feedbackLabel.text = "An unexpected error occurred. Please try again."
+                }
+            }
+        }
+    }
+}
+
     
     /*
     // MARK: - Navigation
@@ -29,4 +68,4 @@ class ForgotPasswordViewController: UIViewController {
     }
     */
 
-}
+
